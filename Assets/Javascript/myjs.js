3318ed7c9d5abf148c9b9c2d9e9bae263465cf4d
Jeff Crump <40723418+eliastwo@users.noreply.github.com>
2018-09-08 21:07:10 -0500

    fixed myjs.js file

--- Assets/Javascript/myjs.js
+++ Assets/Javascript/myjs.js
@@ -3,8 +3,7 @@
 
 var      game = {
 
-	//                           possiblePhrases: ['croquet', 'hyphen', 'swivel', 'ostracize', 'yacht', 'squawk', 'rogue', 'quip', 'phlegm'],
-		  possiblePhrases: ['CROQUET'],
+          possiblePhrases: ['croquet', 'hyphen', 'swivel', 'ostracize', 'yacht', 'squawk', 'rogue', 'quip', 'phlegm'],
 		  gamePhrase: [],
 		  userGuesses: [],
 		  underScores: [],
@@ -12,62 +11,96 @@ var      game = {
 	};
 	
 		  // Data Reset
-		  game.userInput = [];
-		  game.underScores = [];
 		  var wrongGuess = 0;
 		  var correctGuess = 0;
 		  var n = "";
 		  var guessIndex = '';
 		  var firedButton = "";
-		  var isGuessRight = false;
-
-		  //logic to randomly select a phrase for the game
-		  var phraseSelect = game.possiblePhrases[Math.floor(Math.random() * game.possiblePhrases.length)];
-		  game.gamePhrase.push(phraseSelect );
-
+		  var phraseSelect ='';
+		  
+		  	phraseSelect = game.possiblePhrases[Math.floor(Math.random() * game.possiblePhrases.length)];
+			game.gamePhrase.push(phraseSelect );
+			
 		  for(var i=0; i < phraseSelect.length; i++) {
 			game.underScores.push("_ ");
 		  };
-
+		  
 		  $(".underscores").text(game.underScores);
-
-		  $("button").click(function() {
-			 firedButton = $(this).val();
-			 game.userInput.push(firedButton);
-
-		  });
-
-			 $(".guessed").text(game.userInput);
-
-		  var n = phraseSelect.includes(firedButton);
-		  var guessIndex = phraseSelect.indexOf(firedButton);
 		  
-		  if(n === true) {
-			correctGuess = correctGuess + 1;
-			game.underScores.splice(guessIndex,1,firedButton);
+		  	$("button").click(function() {
+			firedButton = $(this).val();
+			game.userInput.push(firedButton);
+			$(".guessed").text(game.userInput);
+
+			n = phraseSelect.includes(firedButton);
+			guessIndex = phraseSelect.indexOf(firedButton);
 			
-			$(".underscores").text(game.underScores);
-			console.log("The value of correctGuess is : " + correctGuess);
-			console.log("The value of guessIndex is : " + guessIndex);
-			console.log("The value of game.underScores is : " + game.underScores);
-		  } else {
-		  	wrongGuess = wrongGuess + 1;
-			console.log("The value of wrongGuess is : " + wrongGuess);
-			$(".start").hide();
-            $(".badguess1").show();
-		  };
+			if(wrongGuess === 7) {
+			
+				alert("computer wins");
+				
+			} else if(correctGuess === phraseSelect.length) {
+			
+				alert("Player Wins");
+			
+			} else {
+			
+				evaluateKey(n, firedButton, guessIndex);
+			};
+			
+});
 		  
-
-/*                         
-                              if(game.gamePhrase.includes(guess)) {
-
-                              correctGuess = correctGuess + 1;
-
-                              } else {
-
-                              wrongGuess = wrongGuess + 1; */
-
-//                           document.getElementById("start").src = '../Images/hangman/badGuess1.jpg';
-
-//            };
+			function evaluateKey(n, firedButton, guessIndex) {
+		 
+			if(n) {
+				correctGuess = correctGuess + 1;
+				game.underScores.splice(guessIndex,1,firedButton);
+				$(".underscores").text(game.underScores);
+				return;
+			
+			} else {
+		 
+				wrongGuess = wrongGuess + 1;
+				
+				switch(wrongGuess) {
+				
+				case 1:
+					$("#start").hide();
+					$("#badguess1").show();
+					return;
+					
+				case 2:
+					$("#badguess1").hide();
+					$("#badguess2").show();
+					return;
+				
+				case 3:
+					$("#badguess2").hide();
+					$("#badguess3").show();
+					return;
+				
+				case 4:
+					$("#badguess3").hide();
+					$("#badguess4").show();
+					return;
+				
+				case 5:
+					$("#badguess4").hide();
+					$("#badguess5").show();
+					return;
+				
+				case 6:
+					$("#badguess5").hide();
+					$("#badguess6").show();
+					return;
+				
+				case 7:
+					$("#badguess6").hide();
+					$("#badguess7").show();
+					return;
+				}
+			};
+		 
+		 };
+		  
          
